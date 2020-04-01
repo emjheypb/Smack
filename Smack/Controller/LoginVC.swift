b93ba071bc2f328520ddb7321060eed6e4304fe6
Mariah Baysic <mjpb123@gmail.com>
2020-04-01 14:43:32 +0800

    added LoginVC, finished LoginVC layout

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Mariah Baysic on 4/1/20.
+//  Copyright © 2020 SpacedOut. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Mariah Baysic on 4/1/20.
+//  Copyright © 2020 SpacedOut. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
