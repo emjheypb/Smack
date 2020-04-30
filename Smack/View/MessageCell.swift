3474a8ed49c8d19badef152491ad76c4510d6b5b
Mariah Baysic <mjpb123@gmail.com>
2020-04-30 20:37:05 +0800

    M: time stamp parsing

--- Smack/View/MessageCell.swift
+++ Smack/View/MessageCell.swift
@@ -21,15 +21,31 @@ class MessageCell: UITableViewCell {
     }
     
     func configureCell (message: Message) {
-        let date = message.timeStamp.split(separator: "T")
-        let time = date[1].split(separator: ".")
-        let hms = time[0].split(separator: ":")
         
         userImage.image = UIImage(named: message.userAvatar)
         userImage.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
         userNameLbl.text = message.userName
-        timeStampLbl.text = "\(date[0]) \(hms[0]):\(hms[1])"
         messageLbl.text = message.message
+        
+//        let date = message.timeStamp.split(separator: "T")
+//        let time = date[1].split(separator: ".")
+//        let hms = time[0].split(separator: ":")
+//
+//        timeStampLbl.text = "\(date[0]) \(hms[0]):\(hms[1])"
+        guard var isoDate = message.timeStamp else { return }
+        let end = isoDate.index(isoDate.endIndex, offsetBy: -5)
+        isoDate = String(isoDate.prefix(upTo: end))
+        
+        let isoFormatter = ISO8601DateFormatter()
+        let chatDate = isoFormatter.date(from: isoDate.appending("Z"))
+        
+        let newFormatter = DateFormatter()
+        newFormatter.dateFormat = "MMM d, h:mm a"
+        
+        if let finalDate = chatDate {
+            let finalDate = newFormatter.string(from: finalDate)
+            timeStampLbl.text = finalDate
+        }
     }
 
 }
